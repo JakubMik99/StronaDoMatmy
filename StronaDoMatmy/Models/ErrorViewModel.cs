cd84ca276001ccff61d1f9dbe4d69d79a464f1d9
Jakub Mikolajczak <jakubmikolajczak1ti@gmail.com>
2022-06-29 13:19:54 +0200

    Dodalem mvc

---
+++ StronaDoMatmy/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StronaDoMatmy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
