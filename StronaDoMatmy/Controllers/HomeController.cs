cea5c31bb5d516d4a3275b2d2fac94fb8b1a9d8d
Jakub Mikolajczak <jakubmikolajczak1ti@gmail.com>
2022-07-01 11:41:17 +0200

    Dodalem wiek na stronie o mnie, dodalem strone do konwertowania systemow liczbowych oraz zrobilem jej wstepny zarys

--- StronaDoMatmy/Controllers/HomeController.cs
+++ StronaDoMatmy/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ public class HomeController : Controller
     {
         return View();
     }
+            public IActionResult Konwerter()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
